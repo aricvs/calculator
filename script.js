ac624691705e755a2e86c932299905f479468090
Ariel Santos <aricvs@gmail.com>
2023-11-04 14:42:21 -0300

    Remove all JS code asides from main operations and currentOperation object

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let currentOperation = { num1: "", operator: "", num2: "" };
-let numberToggle = "numberOne";
 
 function add(num1, num2) {
   return num1 + num2;
@@ -34,61 +33,3 @@ function operate(operator, num1, num2) {
     return divide(num1, num2);
   }
 }
-
-function pressDigitBtn() {
-  const digitBtns = document.querySelectorAll(".digit");
-  const display = document.querySelector(".display-text");
-
-  digitBtns.forEach((button) => {
-    button.addEventListener("click", () => {
-      display.textContent += button.textContent;
-      if (numberToggle === "numberOne") {
-        currentOperation.num1 = Number(display.textContent);
-      }
-      if (numberToggle === "numberTwo") {
-        currentOperation.num2 = Number(display.textContent);
-      }
-    });
-  });
-}
-
-function pressOperationBtn() {
-  const operationBtns = document.querySelectorAll(".operator");
-  const display = document.querySelector(".display-text");
-
-  operationBtns.forEach((button) => {
-    button.addEventListener("click", () => {
-      currentOperation.operator = button.textContent;
-      display.textContent = "";
-      numberToggle = "numberTwo";
-    });
-  });
-}
-
-function pressEqualBtn() {
-  const equalBtn = document.querySelector(".equals");
-  const display = document.querySelector(".display-text");
-  equalBtn.addEventListener("click", () => {
-    const result = operate(
-      currentOperation.operator,
-      currentOperation.num1,
-      currentOperation.num2
-    );
-    display.textContent = result;
-    currentOperation.num1 = Number(display.textContent);
-  });
-}
-
-function pressClearBtn() {
-  const clearBtn = document.querySelector(".clear");
-  clearBtn.addEventListener("click", () => {
-    document.querySelector(".display-text").textContent = "";
-    numberToggle = "numberOne";
-    currentOperation = { num1: "", operator: "", num2: "" };
-  });
-}
-
-pressDigitBtn();
-pressOperationBtn();
-pressEqualBtn();
-pressClearBtn();
